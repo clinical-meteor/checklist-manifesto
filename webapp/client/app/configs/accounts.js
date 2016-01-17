9ead5fdd0f03730eff0beb565390efc1b9ec35ce
Abigail Watson <awatson1978@gmail.com>
2016-01-16 17:34:35 -0800

    removed local packages

--- webapp/client/app/configs/accounts.js
+++ webapp/client/app/configs/accounts.js
@@ -1,21 +1,19 @@
-Meteor.startup(function (){
-  if (Meteor.isClient){
-    ActiveEntry.configure({
-      passwordSignupFields: 'EMAIL_ONLY',
-      logo: {
-        url: "/mini-circles.png",
-        displayed: true
-      },
-      signIn: {
-        displayFullName: true,
-        destination: "/"
-      },
-      signUp: {
-        destination: "/"
-      },
-      themeColors: {
-        primary: ""
-      }
-    });
-  };
-});
+if (Meteor.isClient){
+  ActiveEntry.configure({
+    passwordSignupFields: 'EMAIL_ONLY',
+    logo: {
+      url: "/mini-circles.png",
+      displayed: true
+    },
+    signIn: {
+      displayFullName: true,
+      destination: "/"
+    },
+    signUp: {
+      destination: "/"
+    },
+    themeColors: {
+      primary: ""
+    }
+  });
+};
--- webapp/client/app/configs/theme.js
+++ webapp/client/app/configs/theme.js
@@ -1,15 +1,13 @@
-Meteor.startup(function (){
-  Theme.configure({
-    appTitle: "Checklist Manifesto",
-    background: {
-      color: "#d7eaea"
-    },
-    palette: {
-      colorA: "#CCA79A",
-      colorB: "#831d2c",
-      colorC: "#4D1325",
-      colorD: "#070707",
-      colorE: "#40434E"
-    }
-  });
+Theme.configure({
+  appTitle: "Checklist Manifesto",
+  background: {
+    color: "#d7eaea"
+  },
+  palette: {
+    colorA: "#CCA79A",
+    colorB: "#831d2c",
+    colorC: "#4D1325",
+    colorD: "#070707",
+    colorE: "#40434E"
+  }
 });
