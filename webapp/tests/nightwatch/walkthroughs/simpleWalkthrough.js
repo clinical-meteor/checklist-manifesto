c002f5e370c235f6bd0e29b470c07a7c151bb603
Abigail Watson <awatson1978@gmail.com>
2016-03-06 12:58:44 -0800

    more tweaks to simple walkthrough script

--- webapp/tests/nightwatch/walkthroughs/simpleWalkthrough.js
+++ webapp/tests/nightwatch/walkthroughs/simpleWalkthrough.js
@@ -8,35 +8,39 @@ module.exports = {
       .url("http://localhost:3000/entrySignUp")
       .resizeWindow(1024, 768).pause(500)
       .verify.elementPresent("#entrySignUp")
-      .saveScreenshot("tests/nightwatch/screenshots/ipad/A-signInPage.png")
       .reviewSignUpPage()
+      .saveScreenshot("tests/nightwatch/screenshots/simple/A-signUpPage.png")
       .verify.elementNotPresent("#logoutButton")
-      .signUp("carl@somewhere.com", "carl123").pause(1000)
+      .signUp("carl@somewhere.com", "carl123").pause(1000);
   },
   "B. Home Page": function (client){
     client
       .resizeWindow(1024, 768).pause(500)
       .verify.elementPresent("#checklistPage")
+      .saveScreenshot("tests/nightwatch/screenshots/simple/B-ChecklistPage.png")
       .reviewMainLayout()
-      .reviewSidebar()
+      .reviewSidebar();
   },
   "B. Static Pages": function (client){
     client
       .click("#aboutButton")
       .verify.elementPresent("#aboutPage")
+      .saveScreenshot("tests/nightwatch/screenshots/simple/C-AboutPage.png")
 
       .click("#eulaButton")
       .waitForPage("#eulaPage")
+      .saveScreenshot("tests/nightwatch/screenshots/simple/D-EulaPage.png")
 
       .click("#privacyButton")
-      .verify.elementPresent("#privacyPage")
+      .verify.elementPresent("#privacyPage");
+      .saveScreenshot("tests/nightwatch/screenshots/simple/E-PrivacyPage.png")
   },
   "D. Logout": function(client){
     client
       .verify.elementPresent("#logoutButton")
       .click("#logoutButton").pause(300)
       .verify.containsText("#usernameLink", "Sign In")
-      .verify.elementNotPresent("#logoutButton")
+      .verify.elementNotPresent("#logoutButton");
   },
   after: function(client){
     client
